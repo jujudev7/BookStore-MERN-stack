02b7f512a8019f6f8c94e97ada9caa54b8a40189
jujudev7 <penna.julien@gmail.com>
2024-08-20 11:42:22 +0200

    Get One Book by id with mongoose

--- backend/index.js
+++ backend/index.js
@@ -55,6 +55,20 @@ app.get("/books", async (request, response) => {
   }
 });
 
+// Route for Get One Book from database by id
+app.get("/books/:id", async (request, response) => {
+  try {
+    const { id } = request.params;
+
+    const book = await Book.findById(id);
+
+    return response.status(200).json(book);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
 mongoose
   .connect(mongodbURL)
   .then(() => {
