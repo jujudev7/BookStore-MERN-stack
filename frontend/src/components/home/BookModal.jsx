45c14d97d39b1750bb6ebb3de80ff1f9bff69d15
jujudev7 <penna.julien@gmail.com>
2024-09-04 14:56:26 +0200

    validation props added + width modified

--- frontend/src/components/home/BookModal.jsx
+++ frontend/src/components/home/BookModal.jsx
@@ -1,8 +1,9 @@
+import PropTypes from "prop-types";
 import { AiOutlineClose } from "react-icons/ai";
 import { BiUserCircle } from "react-icons/bi";
 import { PiBookOpenTextLight } from "react-icons/pi";
 
-const BookModal = (book, onClose) => {
+const BookModal = ({ book, onClose }) => {
   return (
     <div
       className="fixed bg-black bg-opacity-60 top-0 left-0 right-0 bottom-0 z-50 flex justify-center items-center"
@@ -10,7 +11,7 @@ const BookModal = (book, onClose) => {
     >
       <div
         onClick={(event) => event.stopPropagation()}
-        className="w-[600px].max-w-full h-[400px] bg-white rounded-xl p-4 flex flex-col relative"
+        className="w-[600px] max-w-full h-[400px] bg-white rounded-xl p-4 flex flex-col relative"
       >
         <AiOutlineClose
           className="absolute right-6 top-6 text-3xl text-red-600 cursor-pointer"
@@ -38,6 +39,17 @@ const BookModal = (book, onClose) => {
       </div>
     </div>
   );
+};
+
+// Validation des props
+BookModal.propTypes = {
+  book: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    publishYear: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+  }).isRequired,
+  onClose: PropTypes.func.isRequired,
 };
 
 export default BookModal;
