84ee2cc1862fe1732bf49f3c10a2cc83184e0eda
jujudev7 <penna.julien@gmail.com>
2024-09-09 15:53:57 +0200

    config.js updated

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,5 @@
-export const PORT = 5555;
+export const PORT = process.env.PORT || 5555;
 
 export const mongodbURL =
+  process.env.MONGO_URI ||
   "mongodb+srv://pennajulien:QX5EgQIYENk53NWO@book-store-mern.ixpamuw.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store-MERN";
